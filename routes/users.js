35659509f0e04bf17ade3efa1d7a462dca7d4a2e
ss1.h18@nemandi.ntv.is <ss1.h18@nemandi.ntv.is>
2019-09-21 19:23:50 +0000

    Next Up: Load User & Set Token

--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,25 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const config = require("config");
-const { check, validationResult } = require("express-validator/check");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator/check');
 
-const User = require("../models/User");
+const User = require('../models/User');
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
 router.post(
-  "/",
+  '/',
   [
-    check("name", "Please add name")
+    check('name', 'Please add name')
       .not()
       .isEmpty(),
-    check("email", "Please include a valid email").isEmail(),
+    check('email', 'Please include a valid email').isEmail(),
     check(
-      "password",
-      "Please enter a password with 6 or mroe characters"
+      'password',
+      'Please enter a password with 6 or mroe characters'
     ).isLength({
       min: 6
     })
@@ -36,7 +36,7 @@ router.post(
       let user = await User.findOne({ email });
 
       if (user) {
-        return res.status(400).json({ msg: "User already exists " });
+        return res.status(400).json({ msg: 'User already exists' });
       }
 
       user = new User({
@@ -59,7 +59,7 @@ router.post(
 
       jwt.sign(
         payload,
-        config.get("jwtSecret"),
+        config.get('jwtSecret'),
         {
           expiresIn: 3600
         },
@@ -70,7 +70,7 @@ router.post(
       );
     } catch (err) {
       console.error(err.message);
-      res.status(500).send("Server Error");
+      res.status(500).send('Server Error');
     }
   }
 );
