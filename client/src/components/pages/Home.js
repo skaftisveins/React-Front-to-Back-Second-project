44d0faa8365e2cc7b5f53556a75b6ab911e2e99f
ss1.h18@nemandi.ntv.is <ss1.h18@nemandi.ntv.is>
2019-09-22 14:16:16 +0000

    React / Express Authentication Done

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,11 +1,19 @@
-import React from "react";
-import Contacts from "../contacts/Contacts";
-import ContactForm from "../contacts/ContactForm";
-import ContactFilter from "../contacts/ContactFilter";
+import React, { useContext, useEffect } from 'react';
+import Contacts from '../contacts/Contacts';
+import ContactForm from '../contacts/ContactForm';
+import ContactFilter from '../contacts/ContactFilter';
+import AuthContext from '../../context/auth/authContext';
 
 const Home = () => {
+  const authContext = useContext(AuthContext);
+
+  useEffect(() => {
+    authContext.loadUser();
+    // eslint-disalbe-next-line
+  }, []);
+
   return (
-    <div className="grid-2">
+    <div className='grid-2'>
       <div>
         <ContactForm />
       </div>
